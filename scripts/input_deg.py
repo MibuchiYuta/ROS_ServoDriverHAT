33741543a0a1e4510656d48385245b5793e44583
mibuchiyuta <6343yuuta@gmail.com>
2021-01-17 16:43:10 +0900

    update

--- scripts/input_deg.py
+++ scripts/input_deg.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+
 import rospy
+
 from std_msgs.msg import Int32
 
 def main():
@@ -8,13 +10,11 @@ def main():
 
     deg = 0
     pulse = 0
-    #rate = rospy.Rate(0.5)
     while not rospy.is_shutdown():
         deg = int(input('deg ='))
         pulse = int(deg * 470 / 180 + 100)
         print(pulse)
         pub.publish(pulse)
-        #rate.sleep()
 
 if __name__ == '__main__':
     try:
